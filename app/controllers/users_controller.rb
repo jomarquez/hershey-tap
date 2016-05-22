52e2966e3f2de5e481c1e56dfb49e24e8c270039
Amy Lai <amyjlai@hotmail.com>
2016-05-22 12:18:32 -0400

    Sign up from web app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(twitter_name: params[:twitter_name])
 
-    if @user.save!
-      render json: @user
+    respond_to do |format|
+      if @user.save!
+        format.html { redirect_to @user, notice: 'You have signed up for Hershey Crush!' }
+        format.json { render @user }
+      end
     end
   end
 
@@ -22,7 +25,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.where(twitter_name: params[:id]).first
+    @user = User.where(twitter_name: params[:id]).first || User.where(id: params[:id]).first
   end
 
   # TODO: get total taps to complete game (yours and your friends)..so we do need a url?
